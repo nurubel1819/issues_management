57cd903dbe25cce2406e987b22725bc6cdce4d95
nurubel1819 <nurubel50@gmail.com>
2026-09-21 09:10:10 +0600

    update role length.

--- src/main/java/com/example/issues_management/auth/entity/User.java
+++ src/main/java/com/example/issues_management/auth/entity/User.java
@@ -33,7 +33,7 @@ public class User implements UserDetails {
 	private String password;
 
 	@Enumerated(EnumType.STRING)
-	@Column(nullable = false, length = 20)
+	@Column(nullable = false, length = 50)
 	private Role role;
 
 	@Column(nullable = false, updatable = false)
